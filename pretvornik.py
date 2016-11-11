460c7a70b9bde740de1ff0e161d4534985b789df
JanezVP <janez.voler@gmail.com>
2016-11-11 12:32:56 +0100

    dodano se iz milj v kilometre

--- pretvornik.py
+++ pretvornik.py
@@ -6,7 +6,7 @@ import tkMessageBox
 window = Tkinter.Tk()
 
 greeting = Tkinter.Label(window,
-                         text="Vnesite kilometre in jaz vam bom izracunal milje.")
+                         text="Pretvornik za kilometre in milje.")
 greeting.pack()
 
 entry_field = Tkinter.Entry(window)
@@ -22,8 +22,21 @@ def check_number(): #spodaj preverimo stevilko
                           milja)
 
 check_button = Tkinter.Button(window,
-                              text="Pretvori",
+                              text="Pretvori kilometre v milje",
                               command=check_number)
+check_button.pack()
+
+
+def check_milje(): #spodaj preverimo stevilko
+    milja = int(entry_field.get())
+    kilometer = milja / 0.62137
+
+    tkMessageBox.showinfo("Kilometer",
+                          kilometer)
+
+check_button = Tkinter.Button(window,
+                              text="Pretvori milje v kilometre",
+                              command=check_milje)
 check_button.pack()
 
 
